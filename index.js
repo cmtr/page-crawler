ec11bba30afdef5d7fb72af1433de024b705c89d
cmtr <24865423+occasionallyharry@users.noreply.github.com>
2019-12-30 01:16:32 +0100

    introduce minimist for command line

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const crawler = require('./src/crawler');
+const minimist = require('minimist')
+const File = require("./src/file");
+const Url = require("./src/url");
+const UrlFile = require("./src/url-file");
+const Crawler = require("./src/crawler");
+
 
 console.log("Start static page generator");
 
+const args = minimist(process.argv/*.slice(2)*/);
+
 const rootUrl = "localhost:3010";
 
-crawler("http://" + rootUrl, "./copy");
+Crawler("http://" + rootUrl, "./copy");
+
+module.exports = {
+	Crawler, File, UrlFile, Url
+};
